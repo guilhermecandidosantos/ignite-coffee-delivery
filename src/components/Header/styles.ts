250bbba4d67851853be908e1d337b66abdc339d1
Guilherme <guilhermecandido1998@gmail.com>
2025-01-02 10:15:23 -0300

    Alter styles

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -87,7 +87,7 @@ export const CardButton = styled.button`
 export const ModalContainer = styled.div`
   position: absolute;
   width: 100%;
-  height: 100%;
+  height: 100vh;
   top: 0;
   left: 0;
   background: rgba(0, 0, 0, 0.4);
--- src/layout/DefaultLayout/style.ts
+++ src/layout/DefaultLayout/style.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
-  margin-bottom: 7rem;
+  /* margin-bottom: 7rem; */
 `
